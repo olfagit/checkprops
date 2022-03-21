39746e7a28c5d1a8d76598174987b0c96ef3f49a
Mohamed Sadok Yahyaoui <mohamedsadokyahyaoui@gmail.com>
2022-03-21 11:13:36 +0100

    first commit

---
+++ src/components/Images.jsx
@@ -0,0 +1,8 @@
+const Images = ({imageUrl})=>{
+return(
+    <div>
+        <img src={imageUrl} alt="profileImage"/>
+    </div>
+);
+}
+export default Images;
---
+++ src/components/Infos.jsx
@@ -0,0 +1,10 @@
+function Infos({fullname,profession,bio}){
+    return(
+<div>
+    <h2>fullname:{fullname}</h2>
+    <h2>bio:{bio}</h2>
+    <h2>profession:{profession}</h2>
+</div>
+    );
+}
+export default Infos;
